18f4a37e95d8d7b85a97dbd3d6936669741a6fdd
Raphael Perez <dotraphael@users.noreply.github.com>
2018-08-01 09:43:43 +0100

    Delete SRV0001.ps1

--- Scripts/SRV0001.ps1
+++
@@ -1,8 +0,0 @@
-Set-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update' -Name AUOptions -Value 1
-
-Start-Process -Filepath ("slmgr") -ArgumentList ("/ato") –wait
-Start-sleep 10
-Start-Process -Filepath ("netsh") -ArgumentList ('interface ip set address "Ethernet" dhcp') –wait
-Start-Process -Filepath ("netsh") -ArgumentList ('interface ip set dns "Ethernet" dhcp') –wait
-
-Add-MpPreference -ThreatIDDefaultAction_Ids @(2147519003) -ThreatIDDefaultAction_Actions Allow
